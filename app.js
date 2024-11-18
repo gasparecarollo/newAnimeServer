bec24f617d89f817fa1283c28b9d8450c0f9b7fa
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-11-18 13:09:21 -0500

    Added Catch All for All in app.js and Put method in CRUD

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const animesController = require("./controllers/animes");
+
+app.get("/", (req, res) => {
+    response.send("Welcome to Animania");
+})
+
+//CRUD
+
+app.use("/animes", animesController);
+
+app.get("*", (req, res) => { //Catch all for all request that did not match any route(methods and paths)
+    res.status(404).send("The request you are looking for does not exist.");
+})
+
+
+module.exports = app;
