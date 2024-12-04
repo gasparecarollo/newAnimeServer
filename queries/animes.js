ff1a6c399fab0aac609e37e5aa381db40debbc9f
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-12-04 13:04:39 -0500

    Added getAllAnimes in queries

--- queries/animes.js
+++ queries/animes.js
@@ -1,2 +1,15 @@
 const db = require("../db/dbConfig");
 
+const getAllAnimes = async () => {
+    try {
+        const animes = await db.any("SELECT * FROM animes");
+        return animes;
+    } catch (error) {
+        return error;
+    }
+}
+
+
+modeule.exports = {
+    getAllAnimes
+}
